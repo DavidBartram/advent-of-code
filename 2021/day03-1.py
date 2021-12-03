1d79a133dd447b86867c1780a5f64f11ff007be8
dbartram <dbartram@dxc.com>
2021-12-03 10:32:53 +0000

    Day 3 solution

--- 2021/day03-1.py
+++ 2021/day03-1.py
@@ -1,28 +1,27 @@
 import sys
+from math import ceil
 
 with open(sys.argv[1]) as file:
     values = file.read().splitlines()
-    newvalues = []
-    for value in values:
-        newvalues.append(list(value))
-    values = newvalues
 
-columns = [[int(values[j][i]) for j in range(len(values))] for i in range(len(values[0]))]
+length = len(values[0])
 
-averages = ''
+ones = [0]*length
 
-flipped = ''
+for value in values:
+    for i in range(length):
+        if value[i] == '1':
+            ones[i] += 1
 
-for column in columns:
-    average_bit = int(round(sum(column)/len(column)))
-    averages += str(average_bit)
-    if average_bit == 1:
-        flipped += '0'
+gamma=''
+epsilon=''
+
+for x in ones:
+    if x > ceil(len(values)/2):
+        gamma += '1'
+        epsilon += '0'
     else:
-        flipped += '1'
+        gamma += '0'
+        epsilon += '1'
 
-gamma = int(averages,2)
-
-epsilon = int(flipped,2)
-
-print(gamma*epsilon)
+print(int(gamma,2)*int(epsilon,2))
--- 2021/day03-2.py
+++ 2021/day03-2.py
@@ -1,43 +1,45 @@
 import sys
+from math import ceil
 
 with open(sys.argv[1]) as file:
     values = file.read().splitlines()
-    newvalues = []
-    for value in values:
-        newvalues.append(list(value))
-    values = newvalues
 
-columns = [[int(values[j][i]) for j in range(len(values))] for i in range(len(values[0]))]
+def count_ones(nums,i):
+    ones = 0
+    for num in nums:
+        if num[i] == '1':
+            ones += 1
+    return ones
 
-averages = ''
-
-flipped = ''
-
-for column in columns:
-    average_bit = int(round(sum(column)/len(column)))
-    averages += str(average_bit)
-    if average_bit == 1:
-        flipped += '0'
-    else:
-        flipped += '1'
-
-values = [''.join(value) for value in values]
-
-def get_rating(comparator, numbers, i):
-        if len(numbers) == 1:
-            return numbers[0]
+def get_rating(nums, i, co2):
+        if len(nums) == 1:
+            return nums[0]
         
         else:
             filtered = []
-            for number in numbers:
-                if number[i] == comparator[i]:
-                    filtered.append(number)
-            numbers = filtered
-            
-            return get_rating(comparator, numbers, i+1)
+            comparator = '0'
+            if co2:
+                comparator = '1'
+            ones = count_ones(nums,i)
+            if ones >= ceil(len(nums)/2):
+                comparator = '1'
+                if co2:
+                    comparator = '0'
 
-print(int(get_rating(averages, values, 0),2))
-print(int(get_rating(flipped, values, 0),2))
+            for num in nums:
+                if num[i] == comparator:
+                    filtered.append(num)
+            
+            nums = filtered
+
+            return get_rating(nums,i+1,co2)
+
+
+o2 = get_rating(values,0, co2=False)
+co2 = get_rating(values,0, co2=True)
+
+print(int(o2,2)*int(co2,2))
+
 
 
 
