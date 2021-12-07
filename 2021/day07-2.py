c2bdccae68edc74b586e1944873f89263d30b4c7
dbartram <dbartram@dxc.com>
2021-12-07 09:56:10 +0000

    Day 7 Solution

---
+++ 2021/day07-2.py
@@ -0,0 +1,26 @@
+import sys
+from statistics import median
+
+with open(sys.argv[1]) as file:
+    data = file.read().split(',')
+    data = [int(val) for val in data]
+
+def sum_fuel_costs(positions, x):
+    sum = 0
+    for position in positions:
+        sum += 0.5*((position-x)**2 + abs(position - x))
+    
+    return int(sum)
+
+min_cost = sum_fuel_costs(data,0)
+
+#brute force solution
+for x in range(min(data),max(data)):
+    cost = sum_fuel_costs(data,x)
+    if cost < min_cost:
+        min_cost = cost
+
+print(min_cost)
+
+
+
