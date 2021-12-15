916ac37875287da7aec3d655e5cffedd3576bd65
dbartram <dbartram@dxc.com>
2021-12-15 13:59:13 +0000

    faster implementation with PriorityQueue

--- 2021/day15-1.py
+++ 2021/day15-1.py
@@ -1,5 +1,7 @@
 import sys
 from collections import defaultdict
+from queue import PriorityQueue
+import math
 
 with open(sys.argv[1]) as file:
     data = file.read().splitlines()
@@ -12,13 +14,16 @@ def initialise(data):
         for i, item in enumerate(row):
             grid[(i,j)] = int(item)
 
-    start = (0,0)
-    unvisited = {start:(start,0)}
-    visited = []
-    distance = {start:0}
-    target = (len(data[0])-1 , len(data[1])-1)
+    visit = PriorityQueue()
+    visit.put((0,(0,0)))
 
-    return grid,distance,unvisited,visited,start,target
+    distance = defaultdict(lambda: math.inf)
+
+    distance[(0,0)] = 0
+
+    target = (len(data[0])-1, len(data)-1)
+
+    return grid,visit,distance, target
 
 def neighbour_coords(point,grid):
 #returns a list of coords of points adjacent to (i,j) in the grid
@@ -33,48 +38,24 @@ def neighbour_coords(point,grid):
         
     return coords_list
 
-def dijkstra(grid,distance,unvisited,visited,node,target):
+def dijkstra(grid,visit,distance,target):
 
-    if node in visited:
-        pass
+    while not visit.empty():
+        (d,(x,y)) = visit.get()
 
-    if node == target:
-        print(distance[target])
-        return None
-
-    for nb in neighbour_coords(node,grid):
-        if nb not in visited:
-            if (not distance.get(nb)) or (distance[node] + grid[nb] < distance.get(nb)):
-                distance[nb] = distance[node] + grid[nb]
-                unvisited[nb] = (nb,distance[node] + grid[nb])
+        for nb in neighbour_coords((x,y), grid):
+            if d + grid[nb] < distance[nb]:
+                distance[nb] = d + grid[nb]
+                visit.put((distance[nb], nb))
     
-    visited.append(node)
-    unvisited.pop(node)
-
-    unvisited = {k: v for k, v in sorted(unvisited.items(), key=lambda item: item[1][1])}
-
-    return next(iter(unvisited))
+    return distance[target]
 
         
 
 
-grid,distance,unvisited,visited,start,target = initialise(data)
+grid,visit,distance, target = initialise(data)
 
-node = start
-
-i = 0
-
-go = True
-
-while go:
-    node = dijkstra(grid,distance,unvisited,visited,node,target)
-
-    if not node:
-        go = False
-
-    i += 1
-    if i%2000 == 0:
-        print(i)
+print(dijkstra(grid,visit,distance,target))
 
 
 
