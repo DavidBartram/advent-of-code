e3343e0d29dd70437fc388b91e838d2670bb36bf
dbartram <dbartram@dxc.com>
2021-12-17 01:04:31 +0000

    day 16 part 1

--- 2021/day16-1.py
+++ 2021/day16-1.py
@@ -1,117 +1,76 @@
+import sys
 
 
-from math import remainder
+class Message:
+    def __init__(self, file):
+        hex_data = file.read()
+        bin_data = bytes.fromhex(hex_data)
 
+        self.pos = 0
+        self.bits = ''
 
-def parseLiteralValue(word, value='', consumed=0):
-
-    if word[0] == '0':
-        consumed += 5
-        value += (word[1:5])
-        word = word[5:]
-        return value, consumed
+        for byte in bin_data:
+            self.bits += '{:08b}'.format(byte)
     
-    else:
-        consumed += 5
-        value += (word[1:5])
-        word = word[5:]
-        value, consumed = parseLiteralValue(word, value, consumed)
-        return value, consumed
+    def integer_value(self, n_bits):
+        result = int(self.bits[self.pos:self.pos+n_bits], 2)
+        self.pos += n_bits
+        return result
+    
+    def parse_one_packet(self):
+        version = self.integer_value(3)
+        id = self.integer_value(3)
+        data = self.read_packet_data(id)
 
-def parseLiteralPacket(word):
+        return (version, id, data)
 
-    value, consumed = parseLiteralValue(word)
+    def read_packet_data(self,id):
+        if id == 4:
+            val = ''
+            go = 1
+            while go == 1:
+                go = self.integer_value(1)
+                val += format(self.integer_value(4),'04b')
 
-    value = int(value,2)
+            return int(val,2)
 
-    print('literal value ', value)
+        else:
+            length_type = self.integer_value(1)
 
-    return value, consumed
+            if length_type == 1:
+                n_packets = self.integer_value(11)
+                output = []
+                for _ in range(n_packets):
+                    output.append(self.parse_one_packet())
 
-def parseOperatorPacket(word, consumed=0):
-
-    length_type = word[0]
-    word = word[1:]
-    consumed += 1
-    print('length_type', length_type)
-
-    if length_type == '0':
-        n_bits = int(word[:15],2)
-        word = word[15:]
-        consumed += 15
-
-        more_consumed = 0
-        while more_consumed < n_bits:
-            x = parseAnyPacket(word)
-            word = word[x:]
-            more_consumed += x
+                return output
             
+            if length_type == 0:
+                x = self.integer_value(15) ##need to do this separately as it changes self.pos
+                end = self.pos + x #end = self.pos + self.integer_value(15) would be bad as it would depend on the order that pos and integer_value are evaluated
+                output=[]
+                while self.pos < end:
+                    output.append(self.parse_one_packet())
 
-        consumed += more_consumed
+                return output
+
+def sum_versions(packet, vsum=0):
+    (version, id, data) = packet
+
+    if id==4:
+        vsum += version
+        return vsum
 
     else:
-        n_packets = int(word[:11],2)
-        word = word[11:]
-        consumed += 11
-        
-        more_consumed = 0
-        packets = 0
-        while packets < n_packets:
-            x = parseAnyPacket(word)
-            word = word[x:]
-            more_consumed += x
-            packets += 1
+        vsum += version
+        for subpacket in data:
+            vsum = sum_versions(subpacket,vsum)
+        return vsum
 
-        consumed += more_consumed
+with open(sys.argv[1]) as file:
+    message = Message(file)
 
-    return consumed
+packet = message.parse_one_packet()
 
-
-def parseAnyPacket(word, consumed=0):
-
-    if len(word) <= 7:
-        return consumed
-
-    version = int(word[:3],2)
-    print('version', version)
-    word = word[3:]
-    consumed +=3
-    id = int(word[:3],2)
-    print('id',id)
-    word = word[3:]
-    consumed += 3
-
-    if id == 4:
-        val,x = parseLiteralPacket(word)
-        word = word[x:]
-        consumed += x
-    
-    else:
-        x = parseOperatorPacket(word)
-        word = word[x:]
-        consumed += x
-
-    return consumed
-
-
-
-
-parseAnyPacket('110100101111111000101000')
-
-print('++++++++++')
-
-parseAnyPacket('00111000000000000110111101000101001010010001001000000000')
-
-print('++++++++++')
-
-parseAnyPacket('11101110000000001101010000001100100000100011000001100000')
-
-
-print('++++++++++')
-
-parseAnyPacket("{0:08b}".format(int('8A004A801A8002F478', 16)))
-
-print('++++++++++')
-
-parseAnyPacket("{0:08b}".format(int('620080001611562C8802118E34', 16)))
+print(sum_versions(packet))
 
