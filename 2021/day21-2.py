b73cc198da0463874880b30f8d32d47cd5997640
dbartram <dbartram@dxc.com>
2021-12-22 09:33:56 +0000

    Use dice result frequencies

--- 2021/day21-2.py
+++ 2021/day21-2.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 from itertools import product
 from functools import lru_cache
 import sys
@@ -6,11 +6,12 @@ import sys
 dice_results = list(product({1,2,3},repeat=3))
 
 dice_results = [sum(result) for result in dice_results]
-print(dice_results)
 
-memo = {}
+dice_freq = dict(Counter(dice_results))
 
-@lru_cache(maxsize=10**5)
+print(dice_freq)
+
+@lru_cache(maxsize=50000)
 def play_dirac_dice(pos1, score1, pos2, score2, whoseTurn):
 
     #print(pos1, score1, pos2, score2, whoseTurn)
@@ -21,23 +22,28 @@ def play_dirac_dice(pos1, score1, pos2, score2, whoseTurn):
     elif score2>=21:
         return 0
     
+    subgame_total = 0
+
     if whoseTurn == 1:
-        newpos = [(pos1+result-1)%10 + 1 for result in dice_results]
-        subgame_results = [play_dirac_dice(pos,score1+pos,pos2,score2,2) for pos in newpos]
-        return sum(subgame_results)
+        for dice in dice_freq:
+            newpos = (pos1+dice-1)%10 + 1
+            subgame_total += dice_freq[dice]*play_dirac_dice(newpos,score1+newpos,pos2,score2,2)
 
     else:
-        newpos = [(pos2+result-1)%10 + 1 for result in dice_results]
-        subgame_results = [play_dirac_dice(pos1,score1,pos,score2+pos,1) for pos in newpos]
-        return sum(subgame_results)
+        for dice in dice_freq:
+            newpos = (pos2+dice-1)%10 + 1
+            subgame_total += dice_freq[dice]*play_dirac_dice(pos1,score1,newpos,score2+newpos,1)
+
+    return subgame_total
 
 start1 = 7
 start2 = 5
 
 wins = (play_dirac_dice(start1,0,start2,0,1), play_dirac_dice(start2,0,start1,0,2))
 
+print(play_dirac_dice.cache_info())
+
 print(max(wins))
-    
 
 
 
