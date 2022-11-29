53d540e0861347c1d1b9f66a1203024058ea2a59
DavidBartram <david.bartram@gmail.com>
2022-11-29 20:47:55 +0000

    2022: set up typescript

---
+++ 2022/build/day1/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const day_1 = require("../day");
+class Day1 extends day_1.Day {
+    constructor() {
+        super(1);
+    }
+    solveForPartOne(input) {
+        return input;
+    }
+    solveForPartTwo(input) {
+        return input;
+    }
+}
+exports.default = new Day1;
