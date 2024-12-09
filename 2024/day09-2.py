c66e64abf7683bcf009b10fadbfd116d93d1a3a0
DavidBartram <david.bartram@gmail.com>
2024-12-09 16:02:05 +0000

    2024 Day 9 Part 2: use deque

--- 2024/day09-2.py
+++ 2024/day09-2.py
@@ -1,5 +1,6 @@
 import os
 from itertools import combinations
+from collections import deque
 
 
 def read_input_file(file_path):
@@ -9,34 +10,31 @@ def read_input_file(file_path):
         input = [int(value) for value in input]
     return input
 
+
 def get_files_and_gaps(input):
     files = {}
     gaps = {}
     pos = 0
     id = 0
-    for i,value in enumerate(input):
+    for i, value in enumerate(input):
         if i % 2 == 0:
             files[pos] = {"id": id, "size": value}
             id += 1
         else:
             gaps[pos] = {"size": value}
-        
+
         pos += value
-    
+
     return files, gaps
 
-def move_file(files,gaps,pos):
-    file_positions = list(files.keys())
-    file_positions.sort(reverse=True)
 
-    gap_positions = list(gaps.keys())
-    gap_positions.sort()
+def move_file(files, gaps, pos):
+    gap_positions = deque(sorted(list(gaps.keys())))
 
     file = files[pos]
 
     while len(gap_positions) > 0:
         gap_pos = gap_positions[0]
-    
 
         if gaps[gap_pos]["size"] >= file["size"] and gap_pos < pos:
             files[gap_pos] = file
@@ -46,38 +44,39 @@ def move_file(files,gaps,pos):
                 new_gap_pos = gap_pos + file["size"]
                 new_gap = {"size": new_gap_size}
                 gaps[new_gap_pos] = new_gap
-                gap_positions.pop(0)
-                gap_positions = [new_gap_pos] + gap_positions
-            
+                gap_positions.popleft()
+                gap_positions.appendleft(new_gap_pos)
+
             del files[pos]
             del gaps[gap_pos]
-        
-            return files,gaps
 
-        gap_positions.pop(0)
-    
-    return files,gaps
+            break
 
-def move_files(files,gaps):
+        gap_positions.popleft()
 
-    positions = list(files.keys())
-    positions.sort(reverse=True)
+    return None
+
+
+def move_files(files, gaps):
+
+    positions = deque(sorted(list(files.keys()), reverse=True))
     while len(positions) > 0:
         if len(positions) % 1000 == 0:
             print(len(positions))
-        files, gaps = move_file(files,gaps,positions[0])
-        positions.pop(0)
-    
-    return files,gaps
+        move_file(files, gaps, positions[0])
+        positions.popleft()
+
+    return None
 
 
 def checksum(files):
     sum = 0
     for pos in files:
-        for k in range(pos,pos+files[pos]["size"]):
+        for k in range(pos, pos + files[pos]["size"]):
             sum += files[pos]["id"] * k
 
     return sum
+
 
 def print_files(files):
     keys = list(files.keys())
@@ -93,9 +92,10 @@ def main():
     input = read_input_file(file_path)
 
     files, gaps = get_files_and_gaps(input)
-    files,gaps = move_files(files, gaps)
+    move_files(files, gaps)
 
     print(checksum(files))
+
 
 if __name__ == "__main__":
     main()
